af42195d911e374cf5ce83d5fe6411cb50635b59
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2022-08-22 11:30:09 -0300

    Swagger aplicado no projeto

---
+++ Tests/Entidades/TestEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Entidades
+{
+    [TestMethod]
+    internal class TestEntity
+    {
+        private string Name { get; set; }
+        private decimal Value { get; set; }
+        private decimal Id { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public decimal GetValue()
+        {
+            return Value;
+        }
+        public decimal GetId()
+        {
+            return Id;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public void SetValue(decimal value)
+        {
+            Value = value;
+        }
+        public void SetId(decimal id)
+        {
+            Id = id;
+        }
+
+    }
+}
